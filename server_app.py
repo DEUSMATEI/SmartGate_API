01d7f5fdc1bd85933ffe1cb35a3a087edc07305e
Deus Matei <deus.matei@gmail.com>
2020-02-17 17:47:00 +0200

    Implemented OpenWeather api call

--- server_app.py
+++ server_app.py
@@ -1,4 +1,6 @@
 from configparser import ConfigParser
+import requests
+import objectpath
 
 #CONFIG DATA 
 CONFIG_REQUIRED_DATA = [
@@ -18,6 +20,28 @@ def loadConfig():
         CONFIG_APIKEY = parser.get('DEFAULT','API_KEY')    
     except:
         print("Can't load config file")
+ 
+def get_weather(apiKey, city, state):
+
+    #get data from openweathermap API
+    url= "https://api.openweathermap.org/data/2.5/weather?q={},{}&units=metric&appid={}".format(city, state, apiKey)   
+    r=requests.get(url)        
+
+    #convert respone to json
+    z= r.json()
+
+    #init search tree
+    jsonnn_tree = objectpath.Tree(z)
+    
+    #init empty dictionary 
+    data = {}
+
+    #filter get only required data
+    for d in CONFIG_REQUIRED_DATA:
+        data[d["name"]] = jsonnn_tree.execute(d["path"])
+            
+    #return required data in Json format
+    return (data)    
     
 #Start Server
 if __name__ == '__main__':
